55e66796ec1d1b2f2a07e11b5cf695b0b5d1a8eb
Jørgen Markussen <jormar@protonmail.com>
2019-05-11 08:53:38 +0200

    Unnamed module

--- modules/oppg1/src/Main.java
+++ modules/oppg1/src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         Logger logger = Logger.getGlobal();
         LogRecord message = new LogRecord(INFO, "Running non-modular code on Java 12");
         logger.log(message);
+
+        Module module = Main.class.getModule();
+        System.out.println("Module: "+module);
+        System.out.println("Name: " + module.getName());
+        System.out.println("isNamed: " + module.isNamed());
+        System.out.println("Descriptor: " + module.getDescriptor());
     }
 }
