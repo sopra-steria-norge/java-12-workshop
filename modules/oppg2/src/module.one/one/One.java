6e39bd62ae73579bae8a47095cb9ef791e77b86f
Jørgen Markussen <jormar@protonmail.com>
2019-05-11 11:07:17 +0200

    Forbedring

--- modules/oppg2/src/module.one/one/One.java
+++ modules/oppg2/src/module.one/one/One.java
@@ -10,5 +10,11 @@ public class One {
         Logger logger = Logger.getGlobal();
         LogRecord message = new LogRecord(INFO, "Today -> " + LocalDateTime.now().toString());
         logger.log(message);
+
+        Module module = new One().getModule();
+        System.out.println("Module: "+module);
+        System.out.println("Name: " + module.getName());
+        System.out.println("isNamed: " + module.isNamed());
+        System.out.println("Descriptor: " + module.getDescriptor());
     }
 }
